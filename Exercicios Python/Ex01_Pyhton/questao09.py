a4b60f188627755093937ae8b924010c65f8af6b
judsonaraujo02 <judsonaraujo@yahoo.com.br>
2017-01-31 18:34:20 -0300

    delete

--- Exercicios Python/Ex01_Pyhton/questao09.py
+++
@@ -1,8 +0,0 @@
-sombraPessoa = float(input("Digite o valor da sombra da pessoa: "))
-sombraPredio = float(input("Digite o valor da sombra do prédio:  "))
-alturaPessoa = float(input("Digite o valor da altura da pessoa:  "))
-
-sombraReal = (sombraPessoa/sombraPredio)
-alturaPredio = (sombraReal*alturaPessoa)
-
-print("A altura do prédio será:  ",alturaPredio)
