c6b1472ba135d816d8d3fa7f47af8585f3821982
judsonaraujo02 <judsonaraujo@yahoo.com.br>
2017-01-31 18:37:19 -0300

    delete

--- Exercicios Python/Ex02_Python/questao02.py
+++
@@ -1,9 +0,0 @@
-def RetornaPorcento():
-
-	qtdReal = float(input("Digite o valor em reais: "))
-
-	valor = (qtdReal * 0.7)
-
-	return "70%  deste valor será: "+str(valor)
-
-print(RetornaPorcento())
